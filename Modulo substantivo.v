3cfecf587d495ad2e7b6b268176af5af3d44ec13
icaro <icaro.moreira@protonmail.com>
2019-11-04 23:07:28 -0300

    2.0

--- Modulo substantivo.v
+++ Modulo substantivo.v
@@ -1,10 +1,12 @@
-module substantivo (clk, Reset, Ready, Tom, Nota, End, Tipo;
+module substantivo (clk, Reset, Ready, Tom, Nota, End, Tipo, Nota_A, Tom_A;
 
     input wire [2:0] Nota_in;
     input wire Tom_in;
+    output wire [1:0] Tipo;
 
+    reg Tom_A;
+    reg [2:0] Nota_A;
     reg [2:0] Nota;
-    reg [1:0] Tipo
     reg Tom, Ready, clk, Reset, End;
 
 
@@ -18,6 +20,8 @@ module substantivo (clk, Reset, Ready, Tom, Nota, End, Tipo;
             Ready <= 1'b0;
             End <= 1'b0;
             Tipo <= 2'b00;
+            Tom_A <= 1'b0;
+            Nota_A <= 1'b000;
             
         end
 
@@ -29,46 +33,85 @@ module substantivo (clk, Reset, Ready, Tom, Nota, End, Tipo;
                 Nota <= Nota_in;            
 
 
-
+                // Verifica se é uma letra inválida, caso sim o algoritmo analisa a entrada anterior
                 if ((Tom == 1'b0 && Nota == 3'b000) || (Tom == 1'b1 && Nota == 3'b000)) begin
                     
                     Ready <= 1'b0;
                     End <= 1'b1;
-                    Tipo <= 2'b00;
+                    
+                    // Verifica a entrada anterior é uma letra válida, caso sim informa qual substantivo é
+                    if ((Tom_A != 1'b0 && Nota_A != 3'b000) || (Tom_A != 1'b1 && Nota_A != 3'b000)) begin
+                        
+                        if (Tom_A == 1'b0 && Nota_A == 3'b011) begin
+
+                            Tipo <= 2'b11;
+
+                        end
+                    
+                        if (Tom_A == 1'b0 && Nota_A == 3'b100) begin
+
+                            Tipo <= 2'b10;
+   
+                        end
+
+                        if (Tom_A == 1'b0 && Nota_A == 3'b101) begin
+                        
+                            Tipo <= 2'b01;
+             
+                        end
+
+                        else begin
+                            
+                            Tipo <= 2'b00;
+
+                        end         
+                        
+                    end
+
+                    // Se a entrada anterior for inválida, se trata de uma palavra inválida
+                    else begin
+                        
+                        Tipo <= 2'b00;
+
+                    end 
                                 
                 end
 
+                // Caso a entrada for uma letra válida o algoritmo vai conferir se é
+                // um Dó, Ré, ou um Mi
                 else begin
-                    
-                    if ((Tom == 1'b0 && Nota == 3'b000) || (Tom == 1'b1 && Nota == 3'b000)) begin
-                        
-                        Tipo <= 2'b00;
-                        Ready <= 1'b0;
-                        End < = 1'b1;
-
-                    end
 
                     if (Tom == 1'b0 && Nota == 3'b011) begin
 
-                        Tipo <= 2'b11;
+                        Tom_A <= Tom;
+                        Nota_A <= Nota;
 
                     end
                     
                     if (Tom == 1'b0 && Nota == 3'b100) begin
 
-                        Tipo <= 2'b10;
+                        Tom_A <= Tom;
+                        Nota_A <= Nota;
    
                     end
 
                     if (Tom == 1'b0 && Nota == 3'b101) begin
                         
-                        Tipo <= 2'b01;
+                        Tom_A <= Tom;
+                        Nota_A <= Nota;
              
                     end
 
+                    // Se for uma letra válida mas diferente, o algoritmo retornará para o estado inicial
+                    else begin
+                        
+                        Tom_A <= Tom;
+                        Nota_A <= Nota;
+
+                    end     
+
 
                 end
-
 
             end
   
